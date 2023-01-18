d6eba335ebe04d666af3b9aef7cc8c86d26397dc
adamx50 <106922156+adamx50@users.noreply.github.com>
2023-01-17 21:37:38 -0300

    Add files via upload

---
+++ Mapa Mental/Sistema Telessaúde.html_files/map.mm
@@ -0,0 +1,258 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#338800" CREATED="1365088062788" ID="ID_1826148032" MODIFIED="1365094678905" TEXT="Sistema Telessa&#xfa;de">
+<font BOLD="true" NAME="SansSerif" SIZE="28"/>
+<node COLOR="#ff0000" CREATED="1365091348803" ID="ID_435463448" MODIFIED="1365095246540" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      TeleDiagn&#243;stico
+    </p>
+    <p>
+      <img src="imagens/tele.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+<node COLOR="#338800" CREATED="1365089646969" FOLDED="true" ID="ID_794463461" MODIFIED="1365097785313">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Teleretinografia
+    </p>
+    <p>
+      <img src="imagens/olho.png" />
+    </p>
+  </body>
+</html></richcontent>
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node COLOR="#990000" CREATED="1365090698311" HGAP="486" ID="ID_146486019" MODIFIED="1365094931594" VSHIFT="-132">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff3333">Campanhas</font>
+    </p>
+    <p>
+      <img src="imagens/retinografo.jpg" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#ff0000" CREATED="1365090731160" HGAP="694" ID="ID_1502877307" MODIFIED="1365094933402" VSHIFT="-82">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pacientes
+    </p>
+    <p>
+      <img src="imagens/paciente.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#ff0000" CREATED="1365090740071" HGAP="635" ID="ID_867937998" MODIFIED="1365094928243" VSHIFT="35">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Laudar
+    </p>
+    <p>
+      <img src="imagens/mac.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#ff0000" CREATED="1365090744007" HGAP="442" ID="ID_1776795473" MODIFIED="1365094927163" VSHIFT="59">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Consultar Laudos
+    </p>
+    <p>
+      <img src="imagens/consultar%20Laudo.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1365089746950" FOLDED="true" HGAP="37" ID="ID_684606697" MODIFIED="1365095408880" VSHIFT="-30">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      TeleCardiologia
+    </p>
+    <p>
+      <img src="imagens/cora?ao.png" />
+    </p>
+  </body>
+</html></richcontent>
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node COLOR="#ff0000" CREATED="1365090902116" HGAP="471" ID="ID_1512137140" MODIFIED="1365094938466" VSHIFT="62">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Exames ECG
+    </p>
+    <p>
+      <img src="imagens/exame.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#338800" CREATED="1365090931740" HGAP="525" ID="ID_41824391" MODIFIED="1365094939858" VSHIFT="69">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Laudar Exames
+    </p>
+    <p>
+      <img src="imagens/mac.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#338800" CREATED="1365091134737" HGAP="437" ID="ID_533970810" MODIFIED="1365094941546" VSHIFT="48">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Consultar Laudos
+    </p>
+    <p>
+      <img src="imagens/consultar%20Laudo.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1365089797806" HGAP="98" ID="ID_115651664" MODIFIED="1365095200990" POSITION="left" VSHIFT="-37">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      TeleEduca&#231;&#227;o
+    </p>
+    <p>
+      <img src="imagens/Tele%20Educacao.png" />
+    </p>
+  </body>
+</html></richcontent>
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+<node COLOR="#338800" CREATED="1365089817419" HGAP="54" ID="ID_965226780" MODIFIED="1365094950105" VSHIFT="-69">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Homem Virtual
+    </p>
+    <p>
+      <img src="imagens/homem%20virtual.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+</node>
+<node COLOR="#990000" CREATED="1365091939283" HGAP="51" ID="ID_944627897" MODIFIED="1365094952945" TEXT="Cursos Ao-Vivo" VSHIFT="-4">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+</node>
+<node COLOR="#338800" CREATED="1365091954722" HGAP="30" ID="ID_1507992303" MODIFIED="1365094951577" TEXT="Cursos Gravados" VSHIFT="68">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1365091739694" HGAP="-24" ID="ID_1672642280" MODIFIED="1365095177664" POSITION="right" VSHIFT="-470">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      TeleAssist&#234;ncia
+    </p>
+    <p>
+      <img src="imagens/Tele%20Assistencia.png" />
+    </p>
+  </body>
+</html></richcontent>
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+<node COLOR="#338800" CREATED="1365089787488" HGAP="122" ID="ID_159944467" MODIFIED="1365094915779" TEXT="TeleConsultorias" VSHIFT="-22">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node COLOR="#990000" CREATED="1365091760411" ID="ID_1707278697" MODIFIED="1365094760077" TEXT="Online">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+</node>
+<node COLOR="#338800" CREATED="1365091764668" ID="ID_335827582" MODIFIED="1365094760077" TEXT="Offline">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#338800" CREATED="1365089767555" HGAP="119" ID="ID_176455164" MODIFIED="1365094913299" VSHIFT="99">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2&#176; Opni&#227;o Formativa
+    </p>
+    <p>
+      <img src="imagens/Segunda%20Opniao.png" />
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1365097831736" ID="ID_1154123113" MODIFIED="1365097887891" POSITION="left" STYLE="fork" TEXT="Farol">
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+<node COLOR="#338800" CREATED="1365098082768" ID="ID_346843911" MODIFIED="1365098122375" TEXT="Munic&#xed;pios">
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#338800" CREATED="1365098089833" ID="ID_1708446606" MODIFIED="1365098123159" TEXT="Estabelecimentos de Sa&#xfa;de">
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+</node>
+</node>
+</node>
+</map>
